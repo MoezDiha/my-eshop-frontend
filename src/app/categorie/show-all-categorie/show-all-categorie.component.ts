d4ef8240c88b270fb95db724d8dfe4d96b817a2f
Ameni Safra <safraameni@gmail.com>
2021-01-22 00:26:52 +0100

    fix show-all-categorie.component.ts.

--- src/app/categorie/show-all-categorie/show-all-categorie.component.ts
+++ src/app/categorie/show-all-categorie/show-all-categorie.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-show-all-categories',
-  templateUrl: './show-all-categories.component.html',
-  styleUrls: ['./show-all-categories.component.css']
+  selector: 'app-show-all-categorie',
+  templateUrl: './show-all-categorie.component.html',
+  styleUrls: ['./show-all-categorie.component.css']
 })
 export class ShowAllCategorieComponent implements OnInit {
 
